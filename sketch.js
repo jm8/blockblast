06b5647b1aefa53682bc1f1762a959a93996430c
Josh <jm8hjkl@gmail.com>
2023-01-05 19:08:51 -0500

    drag and drop

--- sketch.js
+++ sketch.js
@@ -19,60 +19,118 @@ for (let i = 0; i < 8; i++) {
     board[i].push(0);
   }
 }
-board[0][0] = 2;
 
 colors = {
   1: [255, 0, 0],
   2: [0, 0, 255],
 };
 
-const pieces = [
+const cellsForShapeId = [
   [[0, 0], [0, 1], [1, 0], [1, 1]],
 ]
 
-
-
-const drawGridCell = (row, col, spot) => {
-  if (spot == 0) { noFill(); } else {
-    const [r, g, b] = colors[spot];
-    fill(r, g, b);
+const drawGridCell = (row, col, color, transparent = false) => {
+  if (color == 0) { noFill(); } else {
+    const [r, g, b] = colors[color];
+    fill(r, g, b, transparent ? 200 : 255);
   }
   stroke(200);
   strokeWeight(gridWeight);
   rect(gridX + col * (cellSize), gridY + row * (cellSize), cellSize, cellSize);
 }
 
-const drawPiece = (piece, color) => {
+const getPieceSize = (shapeId) => {
+  let pieceWidth = 0, pieceHeight = 0;
+  for (const [row, col] of cellsForShapeId[shapeId]) {
+    pieceWidth = Math.max(pieceWidth, row + 1);
+    pieceHeight = Math.max(pieceHeight, col + 1);
+  }
+  return [pieceWidth, pieceHeight];
+}
+
+const drawPiece = (shapeId, color) => {
   const [r, g, b] = colors[color];
   fill(r, g, b);
   stroke(200);
   strokeWeight(gridWeight);
 
-  let pieceWidth = 0, pieceHeight = 0;
-  for (const [row, col] of pieces[piece]) {
-    pieceWidth = Math.max(pieceWidth, row + 1);
-    pieceHeight = Math.max(pieceHeight, col + 1);
-  }
+  const [pieceWidth, pieceHeight] = getPieceSize(shapeId);
   let x = -(pieceWidth * cellSize) / 2;
   let y = -(pieceHeight * cellSize) / 2;
-  for (const [row, col] of pieces[piece]) {
+
+  for (const [row, col] of cellsForShapeId[shapeId]) {
     rect(x + col * (cellSize), y + row * (cellSize), cellSize, cellSize);
   }
 }
 
-let isActive = false;
-const inactivePieceX = windowWidth / 2;
-const inactivePieceY = windowHeight - 80;
+const getHoveredCells = (shapeId, x, y) => {
+  const [pieceWidth, pieceHeight] = getPieceSize(shapeId);
+  const tlx = x - (pieceWidth * cellSize) / 2;
+  const tly = y - (pieceHeight * cellSize) / 2;
+  const row = Math.floor((tly - gridY + cellSize / 2) / cellSize);
+  const col = Math.floor((tlx - gridX + cellSize / 2) / cellSize);
 
-update = () => {
-  if (isActive && !mouseIsPressed) {
-    isActive = false;
-  } else if (!isActive && mouseIsPressed) {
-    isActive = true;
+  return cellsForShapeId[shapeId].map((([drow, dcol]) => [row + drow, col + dcol]));
+}
+
+const isEmptyCell = (row, col) => {
+  if (row < 0 || row >= 8 || col < 0 || col >= 8) return false;
+  return board[row][col] == 0;
+}
+
+const allEmptyCells = (list) => {
+  for (const [row, col] of list) {
+    if (!isEmptyCell(row, col)) return false;
+  }
+  return true;
+}
+
+let activeOption = null;
+const optionDx = 100;
+const optionXs = [windowWidth / 2 - optionDx, windowWidth / 2, windowWidth / 2 + optionDx];
+const optionsY = windowHeight - 80;
+
+const pieceOptions = [
+  {
+    shapeId: 0,
+    color: 1,
+    used: false,
+  },
+  {
+    shapeId: 0,
+    color: 2,
+    used: false,
+  },
+  {
+    shapeId: 0,
+    color: 1,
+    used: false,
+  }
+];
+
+const update = () => {
+  if (activeOption !== null && !mouseIsPressed) {
+    const hoveredCells = getHoveredCells(0, mouseX, mouseY);
+    if (allEmptyCells(hoveredCells)) {
+      for (const [row, col] of hoveredCells) {
+        board[row][col] = pieceOptions[activeOption].color;
+      }
+      pieceOptions[activeOption].used = true;
+    }
+    activeOption = null;
+  } else if (activeOption === null && mouseIsPressed) {
+    for (let i = 0; i < pieceOptions.length; i++) {
+      if (Math.abs(mouseX - optionXs[i]) < optionDx / 2
+        && Math.abs(mouseY - optionsY) < 50
+        && !pieceOptions[i].used) {
+        activeOption = i;
+      }
+    }
   }
 }
 
 draw = () => {
+  update();
   background(15);
 
   // stroke(200);
@@ -84,14 +142,26 @@ draw = () => {
     }
   }
 
-  push();
-  if (mouseIsPressed) {
+  if (activeOption !== null) {
+    const hoveredCells = getHoveredCells(0, mouseX, mouseY);
+    if (allEmptyCells(hoveredCells)) {
+      for (const [row, col] of hoveredCells) {
+        drawGridCell(row, col, pieceOptions[activeOption].color, true);
+      }
+    }
+    push();
     translate(mouseX, mouseY);
-    drawPiece(0, 1);
-  } else {
-    translate(inactivePieceX, inactivePieceY);
-    scale(0.5);
-    drawPiece(0, 1);
+    drawPiece(pieceOptions[activeOption].shapeId, pieceOptions[activeOption].color);
+    pop();
   }
-  pop();
+
+  for (let i = 0; i < pieceOptions.length; i++) {
+    push();
+    if (i !== activeOption && !pieceOptions[i].used) {
+      translate(optionXs[i], optionsY);
+      scale(0.5);
+      drawPiece(pieceOptions[i].shapeId, pieceOptions[i].color);
+    }
+    pop();
+  }
 }
